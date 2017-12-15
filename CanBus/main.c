97ec26600fe9c4b39f5abe91d0fff95925e88fa9
Zuku271 <adrian.zuchowski@outlook.com>
2017-12-15 20:55:39 +0100

    Added project files

---
+++ CanBus/main.c
@@ -0,0 +1,18 @@
+/*
+ * CanBus.c
+ *
+ * Created: 2017-12-15 20:10:32
+ * Author : Adrian
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
